5c3dfd9d389cbdb2d063835a571b9ba58c303ed0
Mohannad Tbakhi <mohannadtbakhi80@gmail.com>
2021-04-25 23:00:38 +0300

    change some colors for the background

--- main.js
+++ main.js
@@ -1,35 +1,62 @@
 const cards = document.querySelectorAll(".the_card")
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if(lockBoard) return;
+    if(this == firstCard) return;
     this.classList.add('flip');
 
-    if(!hasFlippedCard){
+    if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
 
-    
-    }else{
-        hasFlippedCard = false;
-        secondCard = this;
+        return;
+    }
+    secondCard = this;
 
-        console.log(firstCard.dataset.img)
-        console.log(secondCard.dataset.img)
-        if(firstCard.dataset.img === secondCard.dataset.img){
-            firstCard.removeEventListener("click", flipCard)
-            secondCard.removeEventListener("click", flipCard)
-        }else{
-            setTimeout(() => {
-            firstCard.classList.remove('flip')
-            secondCard.classList.remove('flip')
-            }, 1500);
-        }
-        
-    
+    checkForMAtch();
 }
+
+
+function checkForMAtch() {
+    if (firstCard.dataset.img === secondCard.dataset.img) {
+        disableCards();
+    } else {
+        unflipCards();
+    }
 }
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard)
+    secondCard.removeEventListener("click", flipCard)
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+cards.forEach(card => {
+    let randomPos = Math.floor(Math.random()*12)
+    card.style.order = randomPos;
+})
+})();
+
 cards.forEach(card => card.addEventListener("click", flipCard))
 
 
