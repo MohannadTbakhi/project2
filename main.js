084dadedb2a5951a67c58b3840ec5f572be37815
Mohannad Tbakhi <mohannadtbakhi80@gmail.com>
2021-04-25 05:40:26 +0300

    adding conditional cases

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 const cards = document.querySelectorAll(".the_card")
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+
+        
+    }else{
+        hasFlppedCard = false;
+        secondCard = this;
+
+        console.log({firstCard, secondCard})
+    }
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard))
+
+
+
